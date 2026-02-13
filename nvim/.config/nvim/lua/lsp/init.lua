f268b2565335eac964bd6da9412514f94b671900
Fedora User <user@fedora.local>
2026-02-13 11:42:57 +0000

    add nvim-surround plugin

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -32,35 +32,6 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	end,
 })
 
--- vim.api.nvim_create_autocmd("LspAttach", {
--- 	group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
--- 	callback = function(args)
--- 		local client = vim.lsp.get_client_by_id(args.data.client_id)
--- 		if client == nil then
--- 			return
--- 		end
--- 		if client.name == 'ruff' then
--- 			-- Disable hover in favor of Pyright
--- 			client.server_capabilities.hoverProvider = false
--- 		end
--- 	end,
--- 	desc = 'LSP: Disable hover capability from Ruff',
--- })
-
--- For ruff
--- vim.api.nvim_create_autocmd("BufWritePre", {
--- 	pattern = "*.py",
--- 	callback = function(args)
--- 		vim.lsp.buf.format({
--- 			bufnr = args.buf,
--- 			async = false,
--- 			filter = function(client)
--- 				return client.name == "ruff"
--- 			end,
--- 		})
--- 	end,
--- })
-
 local lsp = {
 	"lua_ls",
 	"tinymist",
@@ -77,3 +48,5 @@ for _, value in ipairs(lsp) do
 	vim.lsp.config(value, lsp_opts)
 	vim.lsp.enable(value)
 end
+
+vim.lsp.inlay_hint.enable(true)
