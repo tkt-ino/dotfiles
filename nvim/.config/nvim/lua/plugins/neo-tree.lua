94276f4c0b061b31bd33f42c6536d828d6caded7
Fedora User <user@fedora.local>
2026-03-14 05:50:59 +0000

    add neo-tree plugin

---
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+		opts = {},
+		keys = {
+			{ "<leader>e", mode = { "n" }, ":Neotree toggle<cr>", silent = true },
+			{ "<leader>b", mode = { "n" }, ":Neotree float buffers<cr>", silent = true },
+		},
+  }
+}
--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,11 +1,11 @@
 return {
-	'stevearc/oil.nvim',
-	---@module 'oil'
-	---@type oil.SetupOpts
-
-	opts = {},
-	keys = {
-		{ "<leader>e", mode = { "n" }, ":Oil<cr>", silent = true },
-	},
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- 'stevearc/oil.nvim',
+	-- ---@module 'oil'
+	-- ---@type oil.SetupOpts
+	--
+	-- opts = {},
+	-- keys = {
+	-- 	{ "<leader>e", mode = { "n" }, ":Oil<cr>", silent = true },
+	-- },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" },
 }
