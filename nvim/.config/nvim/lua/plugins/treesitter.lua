7351c12edad22ec2c88e686bedc8986a8016bec5
tkt-ino <100588602+tkt-ino@users.noreply.github.com>
2026-05-09 20:38:11 +0900

    modify treesitter.lua

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,14 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
+	branch = "main",
 	lazy = false,
-	build = ":TSUpdate"
+	build = ":TSUpdate",
+	-- enabled = false,
+	opts = function(_, opts)
+		require('nvim-treesitter').install { 'rust', 'python', 'c', 'lua', 'typst' }
+		vim.api.nvim_create_autocmd('FileType', {
+			pattern = { 'rust', 'python', 'c', 'lua', 'typst' },
+			callback = function() vim.treesitter.start() end,
+		})
+	end
 }
